a27559102d48fbaf8f3a29a0627cee1118190b9f
JonTDean <JonathanTomDean@Gmail.com>
2020-06-18 18:01:44 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+
+document.getElementById("main").remove(); // Removes main#main
+let newHeader = document.createElement('h1');
+
+function victory(header){
+    newHeader.id = 'victory'
+    newHeader.textContent = 'YOUR-NAME is the champion'
+}
+
+victory(newHeader)
+
+
+// main
