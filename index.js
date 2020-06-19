4ca0fc43798f01a896d9a0bedfa682a7569ba935
darren9897 <darrenkoomson17@gmail.com>
2020-06-19 18:09:01 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+let element = document.getElementById('main');
+element.parentNode.removeChild(element);
+
+let newHeader = document.createElement('h1');
+newHeader.id = "victory"
+
+newHeader.textContent = ("Darren is the champion");
