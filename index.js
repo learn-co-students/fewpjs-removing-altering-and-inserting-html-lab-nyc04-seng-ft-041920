1f040b07468619cfcfcc07ed33f449bb59bf9d2d
aleksarad <alexa.radevich1@gmail.com>
2020-06-20 11:58:16 -0400

    Done.

--- index.js
+++ index.js
@@ -7,4 +7,6 @@ let newHeader = document.createElement('h1');
 newHeader.id = 'victory'
 newHeader.innerHTML = 'YOUR-NAME is the champion!';
 let body = document.querySelector('body');
+
+
 body.appendChild(newHeader)
