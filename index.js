c0514e4b643a1f54c101c99c1707f7f36785f985
hkofkin <hkofkin@gmail.com>
2020-06-22 19:25:17 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+let mainSection = document.querySelector('main');
+mainSection.remove();
+
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = "YOUR-NAME is the champion";
+
+let body = document.querySelector('body');
